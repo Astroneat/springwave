a590b6db3c2e3cbe3af37c00c1965f7190d99ef6
Nguyen Duc Tai <taind.25it@vku.udn.vn>
2026-09-08 23:48:26 +0700

    refactor: fvcking the old landing page

---
+++ js/config/heroSlides.js
@@ -0,0 +1,15 @@
+/**
+ * Danh sách ảnh nền chạy tự động cho Hero Section (Landing Page).
+ *
+ * HƯỚNG DẪN:
+ * - Bạn chỉ cần dán các đường dẫn URL ảnh trực tiếp từ CDN của bạn vào mảng bên dưới.
+ * - Số lượng ảnh do bạn toàn quyền quyết định (2, 3, 5, 10 ảnh...).
+ * - Ảnh sẽ tự động chạy luân phiên mượt mà trên nền Hero.
+ */
+export const HERO_SLIDES = [
+  "https://cdn.springwave.io.vn/activityimg/pic1.jpeg",
+  "https://cdn.springwave.io.vn/activityimg/pic2.jpeg",
+  "https://cdn.springwave.io.vn/activityimg/pic3.jpeg",
+  "https://cdn.springwave.io.vn/activityimg/pic4.jpeg",
+  "https://cdn.springwave.io.vn/activityimg/pic5.jpg"
+];
